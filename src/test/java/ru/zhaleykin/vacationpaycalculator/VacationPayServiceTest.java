906eb0988ca3d07402e8705e0cb7d13ff9cdaf77
Alexey <alexzal235@gmail.com>
2024-03-28 16:13:48 +0400

    add service tests

--- src/test/java/ru/zhaleykin/vacationpaycalculator/VacationPayServiceTest.java
+++ src/test/java/ru/zhaleykin/vacationpaycalculator/VacationPayServiceTest.java
@@ -1,2 +1,41 @@
-package ru.zhaleykin.vacationpaycalculator;public class VacationPayServiceTest {
+package ru.zhaleykin.vacationpaycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.zhaleykin.vacationpaycalculator.service.CalculationVacationPayService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class VacationPayServiceTest {
+
+    @Autowired
+    CalculationVacationPayService calculationVacationPayService;
+
+    @Test
+    void calculateVacationPay(){
+        BigDecimal vac = calculationVacationPayService.calculateVacationPay(1000, 8);
+        assertEquals(vac, BigDecimal.valueOf(273.04));
+    }
+
+    @Test
+    void calculateVacationPayWithDate(){
+        BigDecimal vac =
+                calculationVacationPayService.calculateVacationPay(1000, 8,
+                        LocalDate.of(2024, 1, 9));
+        assertEquals(vac, BigDecimal.valueOf(273.04));
+    }
+
+    @Test
+    void calculateVacationPayOnHolidays(){
+        BigDecimal vac =
+                calculationVacationPayService.calculateVacationPay(1000, 10,
+                        LocalDate.of(2024, 1, 1));
+        assertEquals(vac, BigDecimal.valueOf(68.26));
+    }
 }
